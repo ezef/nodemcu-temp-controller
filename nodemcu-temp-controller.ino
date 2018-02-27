5b5418f81e5ac6c42132a36a1031ab5403f4d1c0
ezequiel Flores <ezequielsebastianflores@gmail.com>
2018-02-27 19:41:46 -0300

    fix relays off on start

--- nodemcu-temp-controller.ino
+++ nodemcu-temp-controller.ino
@@ -320,6 +320,10 @@ void setup(void)
   pinMode(RELAY2, OUTPUT);
   pinMode(RELAY3, OUTPUT);
   pinMode(RELAY4, OUTPUT);
+  digitalWrite(RELAY1,HIGH);
+  digitalWrite(RELAY2,HIGH);
+  digitalWrite(RELAY3,HIGH);
+  digitalWrite(RELAY4,HIGH);
 }
 void getTemps(){
   sensors.requestTemperatures();
